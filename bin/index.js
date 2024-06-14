64ca13c899b46ee820a00b5d6d67c656ad7daa01
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-06-14 16:38:41 +0100

    :sparkles: feat: define 'create project' command

--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,17 @@
 #!/usr/bin/env node
 import arg from 'arg';
+import chalk from 'chalk';
 import logger from "../src/logger.js";
 import start from '../src/commands/start.js';
 import getConfig from '../src/config/config-mgr.js';
+import { createProject } from '../src/commands/create.js';
 
 const logMessage = logger('config:mgr');
 
 try  {
     const args = arg({
         '--start': Boolean,
-        '--build': Boolean,
+        '--create': Boolean,
     });
 
     logMessage.debug('Received args', args);
@@ -18,6 +20,9 @@ try  {
         const configuration = getConfig();
         start(configuration);
     }
+
+    if (args['--create']) { createProject(); }
+
 } catch (error) {
     logMessage.warning(error.message);
     console.log(' ');
@@ -27,6 +32,6 @@ try  {
 
 function usageTool() {
     console.log(`${chalk.whiteBright('tool [CMD]')}
-    ${chalk.greenBright('--start')}\tStarts the app
-    ${chalk.greenBright('--build')}\tBuilds the app`);
+    ${chalk.greenBright('--start')}\tStarts the project
+    ${chalk.greenBright('--create')}\tCreate the project`);
 }
