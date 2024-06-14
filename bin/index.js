2a8e0a2cf0714d9facd3743039153e116326e02e
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-06-14 14:43:23 +0100

    :sparkles: create the 'base' for the command line tool (tests only)

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,23 @@
 #!/usr/bin/env node
-console.log(process.argv);
+const arg = require('arg')
+
+try  {
+    const args = arg({
+        '--start': Boolean,
+        '--build': Boolean,
+    });
+    
+    if (args['--start'])
+        console.log('starting the app');
+} catch (error) {
+    console.log(error.message);
+    console.log(' ');
+
+    usageTool();
+}
+
+function usageTool() {
+    console.log(`tool [CMD]
+    --start\tStarts the app
+    --build\tBuilds the app`);
+}
