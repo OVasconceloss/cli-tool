ab1c6aadd1852206e98818274790b6a041e7ac46
OVasconceloss <victorolimpiovasconcelos2006@gmail.com>
2024-06-14 15:28:43 +0100

    :sparkles: feat: change the logic to use user config

--- src/config/config-mgr.js
+++ src/config/config-mgr.js
@@ -6,13 +6,11 @@ const configLoader = cosmiconfigSync('tool');
 export default function getConfig() {
     const resultConfig = configLoader.search(process.cwd());
 
-    if (resultConfig) {
-        console.log('Found configuration (package.json file)', resultConfig.config);
-        return packageConfig.tool;
-    } else if (hasJSConfigFile()) {
-        return loadJSConfigFile();
-    } else {
+    if (!resultConfig) {
         console.log(chalk.yellow('Could not find configuration, using default'));
         return { port: 8080 };
+    } else {
+        console.log('Found configuration (package.json file)', resultConfig.config);
+        return resultConfig.config;
     }
 }
